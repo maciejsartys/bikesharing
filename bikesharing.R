23c32f567c6e5f5a0b8e403458f252dc260889f7
Maciej Sartys <sartys.maciej@gmail.com>
2018-02-27 19:37:44 +0100

    save_submission function

--- bikesharing.R
+++ bikesharing.R
@@ -7,39 +7,52 @@ library(forcats)
 # ml packages
 library(MLmetrics)
 
-#data import
+# data import
 train_data_file <- "data/train.csv"
 test_data_file <- "data/test.csv"
 sample_submission_file <- "data/sampleSubmission.csv"
-submissions_dir <- "./submissions/"
 
 train.df <- read_csv(train_data_file)
 test.df <- read_csv(test_data_file)
 sample_submission.df <- read_csv(sample_submission_file)
 
 # categorical variables as factors conversion
-train.df %>% mutate(season =
-                      as_factor(as.character(season),
-                             levels = 1:4,
-                             labels = c("spring", "summer", "fall", "winter")
-                             ),
-                    weather =
-                      as_factor(as.character(weather),
-                             levels = 1:4,
-                             labels = c("clear", "mist", "light rain/snow", "heavy rain/snow")
-                             )
-                    ) -> train.df
+train.df %>% mutate(
+  season =
+    as_factor(
+      as.character(season),
+      levels = 1:4,
+      labels = c("spring", "summer", "fall", "winter")
+    ),
+  weather =
+    as_factor(
+      as.character(weather),
+      levels = 1:4,
+      labels = c("clear", "mist", "light rain/snow", "heavy rain/snow")
+    )
+) -> train.df
 
-#baseline dummy model
+# 
+save_submission <- function(x, y, filename) {
+  submissions_dir <- "./submissions/"
+  submission <- tibble(datetime = x, count = y)
+  write_csv(submission, paste0(submissions_dir, filename, ".csv"))
+  return(TRUE)
+}
+
+# baseline dummy model
 
 mean_count <- round(mean(train.df$count))
 
 train.df %>% select(datetime) %>% mutate(
-  count = mean_count) -> train_pred
+  count = mean_count
+) -> train_pred
 RMSLE(train_pred$count, train.df$count)
-#1.569198
+# 1.569198
 
 test.df %>% select(datetime) %>% mutate(
   datetime = as.character(datetime),
-  count = mean_count) -> test_pred
-write_csv(test_pred, paste0(submissions_dir, '01_dummy_mean.csv'))
+  count = mean_count
+) -> test_pred
+save_submission(test.df$datetime, test_pred$count, "01_dummy_mean")
+# 1.58555
